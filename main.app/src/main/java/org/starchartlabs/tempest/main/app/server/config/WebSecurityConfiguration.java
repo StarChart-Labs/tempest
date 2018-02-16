b304b838e3aca2c799af6d3fd693ee7ce477ec88
Ryan O'Meara <romeara@live.com>
2018-02-16 00:10:46 -0500

    Get basic authentication with Google. Username read needs some work

---
+++ main.app/src/main/java/org/starchartlabs/tempest/main/app/server/config/WebSecurityConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Feb 15, 2018 StarChart Labs Authors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ryan - initial API and implementation and/or initial documentation
+ */
+package org.starchartlabs.tempest.main.app.server.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableOAuth2Sso
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf()
+                .disable()
+        .antMatcher("/**")
+        .authorizeRequests()
+        .antMatchers("/", "/index.html")
+                .permitAll()
+        .anyRequest()
+                .authenticated();
+    }
+
+}
